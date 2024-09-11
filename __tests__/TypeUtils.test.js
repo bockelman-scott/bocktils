3a7e54bd34679f596c1f475cd0b2637989364b8f
bockelman-scott <152332497+bockelman-scott@users.noreply.github.com>
2024-09-11 17:12:49 -0500

    added more tests

--- __tests__/TypeUtils.test.js
+++ __tests__/TypeUtils.test.js
@@ -12,6 +12,11 @@ const stringUtils = require( "../utils/StringUtils" );
 let VALID_TYPES = typeUtils.VALID_TYPES;
 let JS_TYPES = typeUtils.JS_TYPES;
 
+const anAsyncFunction = async function()
+{
+    await console.log( "When I get around to it..." );
+};
+
 class A
 {
     constructor()
@@ -35,6 +40,11 @@ class A
     static doNothing()
     {
         console.log( "\"I didn't do it.  No one saw me do it.  You can't prove anything\" -- Bart Simpson" );
+    }
+
+    async lazyLog()
+    {
+        await anAsyncFunction();
     }
 }
 
@@ -86,6 +96,19 @@ test( "JS_TYPES.includes('string') && JS_TYPES.includes('number') && JS_TYPES.in
           expect( actual ).toBe( true );
       } );
 
+test( "isObject(null) === true",
+      () =>
+      {
+          let obj = null;
+          expect( typeUtils.isObject( obj ) ).toBe( true );
+      } );
+
+test( "isObject(null,{rejectNull:true) === false",
+      () =>
+      {
+          let obj = null;
+          expect( typeUtils.isObject( obj, { rejectNull: true } ) ).toBe( false );
+      } );
 
 test( "isObject({}) === true",
       () =>
@@ -230,9 +253,114 @@ test( "isFunction(new C().makeFunction()()) === false",
           expect( typeUtils.isFunction( obj ) ).toBe( false );
       } );
 
+test( "isAsyncFunction(anAsyncFunction) === true",
+      () =>
+      {
+          expect( typeUtils.isAsyncFunction( anAsyncFunction ) ).toBe( true );
+      } );
+
+
+test( "isAsyncFunction(anAsyncFunction()) === false",
+      () =>
+      {
+          expect( typeUtils.isAsyncFunction( anAsyncFunction() ) ).toBe( false );
+      } );
+
+test( "isAsyncFunction(C) === false",
+      () =>
+      {
+          expect( typeUtils.isAsyncFunction( C ) ).toBe( false );
+      } );
+
+test( "isAsyncFunction(new C().lazyLog) === true",
+      () =>
+      {
+          expect( typeUtils.isAsyncFunction( new C().lazyLog ) ).toBe( true );
+      } );
+
+test( "isAsyncFunction(new C().lazyLog()) === false",
+      () =>
+      {
+          expect( typeUtils.isAsyncFunction( new C().lazyLog() ) ).toBe( false );
+      } );
+
 test( "isClass(C) === true",
       () =>
       {
           let obj = C;
           expect( typeUtils.isClass( obj ) ).toBe( true );
+      } );
+
+
+test( "isString('abc') === true",
+      () =>
+      {
+          expect( typeUtils.isString( "abc" ) ).toBe( true );
+      } );
+
+test( "isString(new String('abc')) === true",
+      () =>
+      {
+          expect( typeUtils.isString( new String( "abc" ) ) ).toBe( true );
+      } );
+
+test( "isString(0) === false",
+      () =>
+      {
+          expect( typeUtils.isString( 0 ) ).toBe( false );
+      } );
+
+test( "isString(['abc']) === false",
+      () =>
+      {
+          expect( typeUtils.isString( ["abc"] ) ).toBe( false );
+      } );
+
+test( "isString() === false",
+      () =>
+      {
+          expect( typeUtils.isString() ).toBe( false );
+      } );
+
+test( "isString(null) === false",
+      () =>
+      {
+          expect( typeUtils.isString( null ) ).toBe( false );
+      } );
+
+test( "isString(undefined) === false",
+      () =>
+      {
+          expect( typeUtils.isString( undefined ) ).toBe( false );
+      } );
+
+
+test( "isNumber(0) === true",
+      () =>
+      {
+          expect( typeUtils.isNumber( 0 ) ).toBe( true );
+      } );
+
+test( "isNumber(20000000000000000000000000000000000000000000000000000000000000000000000n) === true",
+      () =>
+      {
+          expect( typeUtils.isNumber( 20000000000000000000000000000000000000000000000000000000000000000000000n ) ).toBe( true );
+      } );
+
+test( "isNumber('123') === false",
+      () =>
+      {
+          expect( typeUtils.isNumber( "123" ) ).toBe( false );
+      } );
+
+test( "isNumber(new Number('123')) === true",
+      () =>
+      {
+          expect( typeUtils.isNumber( new Number( "123" ) ) ).toBe( true );
+      } );
+
+test( "isNumeric('123') === true",
+      () =>
+      {
+          expect( typeUtils.isNumeric( "123" ) ).toBe( true );
       } );
