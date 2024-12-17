ea6a377732b872ea0d1378e7fe129e3771dc96a9
bockelman-scott <152332497+bockelman-scott@users.noreply.github.com>
2024-12-17 00:01:09 -0600

    Added package.json for each package directory
    Preparing to manage and publish with Lerna

---
+++ packages/dates/index.js
@@ -0,0 +1,180 @@
+const core = require( "@toolbocks/core" );
+
+const commonUtils = require( "@toolbocks/common" );
+
+/**
+ * Establish separate constants for each of the common utilities imported
+ * @see ../src/CommonUtils.cjs
+ */
+const { constants, typeUtils, stringUtils, arrayUtils, funcUtils, localeUtils } = commonUtils;
+
+const dateUtils = require( "./src/DateUtils.cjs" );
+const dateFormatTokenUtils = require( "./src/DateFormatTokenSet.cjs" );
+const dateFormatterUtils = require( "./src/DateFormatter.cjs" );
+const dateParserUtils = require( "./src/DateParser.cjs" );
+
+const { _ud = "undefined" } = constants;
+
+const $scope = core?.$scope || constants?.$scope || function()
+{
+    return (_ud === typeof self ? ((_ud === typeof global) ? ((_ud === typeof globalThis ? {} : globalThis)) : (global || {})) : (self || {}));
+};
+
+(function exposeModule()
+{
+    const INTERNAL_NAME = "__BOCK__DATES_PACKAGE__";
+
+    if ( $scope() && (null != $scope()[INTERNAL_NAME]) )
+    {
+        return $scope()[INTERNAL_NAME];
+    }
+
+    const dependencies =
+        {
+            constants,
+            typeUtils,
+            stringUtils,
+            arrayUtils,
+            funcUtils,
+            localeUtils,
+            dateUtils,
+            dateFormatTokenUtils,
+            dateFormatterUtils,
+            dateParserUtils
+        };
+
+    const { classes } = constants;
+
+    const { Result, isDate } = typeUtils;
+
+    const { ModuleEvent, ModulePrototype } = classes;
+
+    if ( _ud === typeof CustomEvent )
+    {
+        CustomEvent = ModuleEvent;
+    }
+
+    const
+        {
+            DateConstants,
+            DateFilters,
+            isValidDateArgument,
+            avoidWeekend,
+            addDays,
+            addWorkdays,
+            addWeeks,
+            subtractDays,
+            subtractWeeks,
+            toNoon,
+            toMidnight,
+            firstInstant,
+            lastInstant,
+            before,
+            after,
+            equal,
+            sortDates,
+            earliest,
+            latest,
+            daysBetween,
+            workDaysBetween
+        } = dateUtils;
+
+    const
+        {
+            resolveDate,
+            resolveLocale,
+            DEFAULT_LOCALE,
+            DEFAULT_TOKEN_SET,
+            DEFAULT_FORMAT,
+            DateFormatter
+        } = dateFormatTokenUtils;
+
+    const { DateParser } = dateParserUtils;
+
+    const
+        {
+            getMonthNames,
+            getMonthAbbreviations,
+            getDayNames,
+            getDayAbbreviations,
+            getDayLetters,
+            getEras,
+            getAmPmStrings,
+            getFirstDayOfWeek,
+            FORMATS
+        } = localeUtils;
+
+    const modName = "DatesPackage";
+
+    const modulePrototype = new ModulePrototype( modName, INTERNAL_NAME );
+
+    let mod =
+        {
+            dependencies: dependencies,
+
+            DateUtils: dateUtils,
+            DateFormatTokenSetUtils: dateFormatTokenUtils,
+            DateFormatterUtils: dateFormatterUtils,
+            DateParserUtils: dateParserUtils,
+
+            classes:
+                {
+                    DateFormatter,
+                    DateParser
+                },
+            DateFormatter,
+            DateParser,
+
+            Result,
+            isDate,
+
+            DateConstants,
+            DateFilters,
+
+            isValidDateArgument,
+            avoidWeekend,
+            addDays,
+            addWorkdays,
+            addWeeks,
+            subtractDays,
+            subtractWeeks,
+            toNoon,
+            toMidnight,
+            firstInstant,
+            lastInstant,
+            before,
+            after,
+            equal,
+            sortDates,
+            earliest,
+            latest,
+            daysBetween,
+            workDaysBetween,
+
+            getMonthNames,
+            getMonthAbbreviations,
+
+            getDayNames,
+            getDayAbbreviations,
+            getDayLetters,
+
+            getEras,
+
+            getAmPmStrings,
+
+            getFirstDayOfWeek,
+
+            FORMATS,
+
+            resolveDate,
+            resolveLocale,
+
+            DEFAULT_LOCALE,
+            DEFAULT_TOKEN_SET,
+            DEFAULT_FORMAT
+        };
+
+    mod = modulePrototype.extend( mod );
+
+    return mod.expose( mod, INTERNAL_NAME, (_ud !== typeof module ? module : mod) ) || mod;
+}());
