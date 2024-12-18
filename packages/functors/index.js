57be91520ae9b3c804796283a2d07a2c1f2481f4
bockelman-scott <152332497+bockelman-scott@users.noreply.github.com>
2024-12-18 16:06:29 -0600

    Updated dev dependencies
    added JsDoc and monorepodoc

---
+++ packages/functors/index.js
@@ -0,0 +1,64 @@
+const commonUtils = require( "@toolbocks/common" );
+
+const { constants, typeUtils, stringUtils, arrayUtils, objectUtils } = commonUtils;
+
+const objectFunctorUtils = require( "./src/ObjectFunctor.cjs" );
+
+const { ObjectFunctor } = objectFunctorUtils;
+
+const iteratorUtils = require( "./src/IteratorUtils.cjs" );
+
+/** define a variable for typeof undefined **/
+const { _ud = "undefined", classes } = constants;
+
+/**
+ * This function returns the host environment scope (Browser window, Node.js global, or Worker self)
+ */
+const $scope = constants?.$scope || function()
+{
+    return (_ud === typeof self ? ((_ud === typeof global) ? {} : (global || {})) : (self || {}));
+};
+
+/**
+ * This is the Immediately Invoked Function Expression (IIFE) that builds and returns the module
+ */
+(function exposeModule()
+{
+    // defines a key we can use to store this module in global scope
+    const INTERNAL_NAME = "__BOCK__FUNCTOR_UTILS__";
+
+    // if we've already executed this code, just return the module
+    if ( $scope() && (null != $scope()[INTERNAL_NAME]) )
+    {
+        return $scope()[INTERNAL_NAME];
+    }
+
+    const dependencies =
+        {
+            constants,
+            typeUtils,
+            stringUtils,
+            arrayUtils,
+            objectUtils,
+            ObjectFunctor,
+            iteratorUtils
+        };
+
+    const modName = "FunctorUtils";
+
+    const { ModulePrototype } = classes;
+
+    const modulePrototype = new ModulePrototype( modName, INTERNAL_NAME );
+
+    let mod =
+        {
+            dependencies,
+            ObjectFunctor,
+            iteratorUtils,
+        };
+
+    mod = modulePrototype.extend( mod );
+
+    return mod.expose( mod, INTERNAL_NAME, (_ud !== typeof module ? module : mod) ) || mod;
+
+}());
