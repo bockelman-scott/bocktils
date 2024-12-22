11c03d63dbe4510232273c3bd25969275090a0d0
bockelman-scott <152332497+bockelman-scott@users.noreply.github.com>
2024-12-21 21:04:56 -0600

    Implemented 3 basic MathUtils functions
    resolveNaN, isBetween, and quotient (a safe division)

--- packages/math/__tests__/MathUtils.test.js
+++ packages/math/__tests__/MathUtils.test.js
@@ -1,13 +1,157 @@
-// no need to require jest here... run this test from the console using 'npx jest'
-// let jester = require( "jest" );
-// jester.run( __filename );
 const mathUtils = require( "../src/MathUtils.cjs" );
 
-describe( "Tests", () =>
+const { resolveNullOrNaN, isBetween, quotient } = mathUtils;
+
+mathUtils.disableLogging();
+
+describe( "resolveNullOrNaN", () =>
 {
-    test( "Test",
+    test( "resolveNullOrNaN for valid argument, number",
           () =>
           {
-              expect( 1 ).toEqual( 1 );
+              expect( resolveNullOrNaN( 1 ) ).toEqual( 1 );
+              expect( resolveNullOrNaN( 0.23 ) ).toEqual( 0.23 );
+              expect( resolveNullOrNaN( -1 ) ).toEqual( -1 );
+              expect( resolveNullOrNaN( 0xFF ) ).toEqual( 255 );
+          } );
+
+    test( "resolveNullOrNaN for valid argument, numeric string",
+          () =>
+          {
+              expect( resolveNullOrNaN( "1" ) ).toEqual( 1 );
+              expect( resolveNullOrNaN( "0.23" ) ).toEqual( 0.23 );
+              expect( resolveNullOrNaN( "-1" ) ).toEqual( -1 );
+              expect( resolveNullOrNaN( "0xFF" ) ).toEqual( 255 );
+          } );
+
+    test( "resolveNullOrNaN for invalid argument, non-numeric string",
+          () =>
+          {
+              expect( resolveNullOrNaN( "abc" ) ).toEqual( 0 );
+              expect( resolveNullOrNaN( "" ) ).toEqual( 0 );
+              expect( resolveNullOrNaN( "---" ) ).toEqual( 0 );
+              expect( resolveNullOrNaN( "0xFG" ) ).toEqual( 0 );
+
+              // now with default values
+
+              expect( resolveNullOrNaN( "abc", 9 ) ).toEqual( 9 );
+              expect( resolveNullOrNaN( "", 7 ) ).toEqual( 7 );
+              expect( resolveNullOrNaN( "---", 23 ) ).toEqual( 23 );
+              expect( resolveNullOrNaN( "0xFG", 15 ) ).toEqual( 15 );
+
+          } );
+
+    test( "resolveNullOrNaN for invalid argument, non-numeric values",
+          () =>
+          {
+              expect( resolveNullOrNaN( [] ) ).toEqual( 0 );
+              expect( resolveNullOrNaN( {} ) ).toEqual( 0 );
+              expect( resolveNullOrNaN( [""] ) ).toEqual( 0 );
+              expect( resolveNullOrNaN( ["---", "+++"] ) ).toEqual( 0 );
+              expect( resolveNullOrNaN( ["0xFG"] ) ).toEqual( 0 );
+
+              // now with default values
+
+              const obj = { a: 1, b: 2 };
+
+              expect( resolveNullOrNaN( obj, Object.keys( obj ).length ) ).toEqual( 2 );
+              expect( resolveNullOrNaN( ["abc"], 9 ) ).toEqual( 9 );
+              expect( resolveNullOrNaN( [""], 7 ) ).toEqual( 7 );
+              expect( resolveNullOrNaN( ["---", "+++"], 23 ) ).toEqual( 23 );
+              expect( resolveNullOrNaN( ["0xFG"], 15 ) ).toEqual( 15 );
+
+          } );
+
+    test( "resolveNullOrNaN for NaN or Infinity",
+          () =>
+          {
+              expect( resolveNullOrNaN( NaN ) ).toEqual( 0 );
+              expect( resolveNullOrNaN( Infinity ) ).toEqual( 0 );
+              expect( resolveNullOrNaN( -Infinity ) ).toEqual( 0 );
+              expect( resolveNullOrNaN( 1 / 0 ) ).toEqual( 0 );
+
+              expect( resolveNullOrNaN( NaN, 1 ) ).toEqual( 1 );
+              expect( resolveNullOrNaN( Infinity, Number.MAX_VALUE ) ).toEqual( Number.MAX_VALUE );
+              expect( resolveNullOrNaN( -Infinity, Number.MIN_VALUE ) ).toEqual( Number.MIN_VALUE );
+              expect( resolveNullOrNaN( 1 / 0, Infinity ) ).toEqual( 0 );
+              expect( resolveNullOrNaN( 1 / 0, 777 ) ).toEqual( 777 );
+          } );
+} );
+
+describe( "isBetween", () =>
+{
+    test( "0 isBetween -1 and 1",
+          () =>
+          {
+              expect( isBetween( 0, -1, 1 ) ).toBe( true );
+          } );
+
+    test( "1 isBetween -1 and 1",
+          () =>
+          {
+              expect( isBetween( 1, -1, 1 ) ).toBe( true );
+          } );
+
+    test( "1 isBetween -1 and 1, exclusive is false",
+          () =>
+          {
+              expect( isBetween( 1, -1, 1, false ) ).toBe( false );
+          } );
+} );
+
+describe( "quotient", () =>
+{
+    test( "quotient (is a safe division)",
+          () =>
+          {
+              let x = 1;
+              let y = 0;
+
+              let z = quotient( x, y );
+
+              expect( z ).toEqual( 0 );
+
+              z = quotient( x, y, { byZero: 17 } );
+
+              expect( z ).toEqual( 17 );
+
+              z = quotient( x, y, { byZero: Infinity } );
+
+              expect( z ).toEqual( 0 );
+
+              z = quotient( x, y, { defaultQuotient: 23 } );
+
+              expect( z ).toEqual( 23 );
+
+              z = quotient( 0, y );
+
+              expect( z ).toEqual( 0 );
+
+              y = 1;
+
+              z = quotient( x, y );
+
+              expect( z ).toEqual( 1 );
+
+              x = NaN;
+
+              z = quotient( x, y );
+
+              expect( z ).toEqual( 1 );
+
+
+              x = 12;
+
+              y = NaN;
+
+              z = quotient( x, y );
+
+              expect( z ).toEqual( 12 );
+
+
+              z = quotient( x, y, { defaultDivisor: 23 } );
+
+              expect( z ).toEqual( 12/23 );
+
           } );
 } );
--- packages/math/src/MathUtils.cjs
+++ packages/math/src/MathUtils.cjs
@@ -1,10 +1,8 @@
-/** import dependencies **/
-
 const core = require( "@toolbocks/core" );
 
 const { constants, typeUtils, stringUtils, arrayUtils } = core;
 
-/** define a variable for typeof undefined **/
+/* define a variable for typeof undefined **/
 const { _ud = "undefined" } = constants;
 
 /**
@@ -27,10 +25,14 @@ const $scope = constants?.$scope || function()
     }
 
     /**
-     * An array of this module's dependencies
-     * which are re-exported with this module,
-     * so if you want to, you can just import the leaf module
-     * and then use the other utilities as properties of that module
+     * This is a dictionary of this module's dependencies.
+     * <br>
+     * It is exported as a property of this module,
+     * allowing us to just import a leaf module
+     * and then use the other utilities as properties of that module.
+     * <br>
+     * @dict
+     * @type {Object}
      */
     const dependencies =
         {
@@ -40,7 +42,7 @@ const $scope = constants?.$scope || function()
             arrayUtils
         };
 
-    /**
+    /*
      * Create local variables for the imported values and functions we use.
      */
     const {
@@ -94,9 +96,169 @@ const $scope = constants?.$scope || function()
         CustomEvent = ModuleEvent;
     }
 
+    /**
+     * This is the error message used when a non-numeric argument is passed to a method expecting a number.
+     * @const
+     * @type {string}
+     */
+    const ERROR_MSG_NON_NUMERIC = "This function requires a numeric argument";
+
+    /**
+     * This is the error message used when division by zero is detected.
+     * @const
+     * @type {string}
+     */
+    const ERROR_MSG_DIVISION_BY_ZERO = "Division by Zero Detected.  Returning Default Quotient";
+
+    /**
+     * This is the name of this module
+     * @const
+     * @type {string}
+     */
     const modName = "MathUtils";
 
+    /**
+     * This is the object that is returned from this function.
+     * <br>
+     * This object is the MathUtils module.<br>
+     * <br>
+     * The functions defined in this file are added to the module before it is exported and returned.
+     * <br>
+     * @type {ModulePrototype}
+     * @module MathUtils
+     */
     const modulePrototype = new ModulePrototype( modName, INTERNAL_NAME );
+
+    /**
+     * Returns a valid number regardless of its argument.<br>
+     * <br>
+     * If the argument specified is a number<br>
+     * (or a string that can be coerced to a number),<br>
+     * and that number is not NaN or Infinity,<br>
+     * that number is returned unmodified.<br>
+     * <br>
+     * If the argument specified is not numeric or is NaN or Infinity,<br>
+     * the specified default is returned (subject to the same criteria).<br>
+     * <br>
+     * If the default specified is also NaN or Infinity,<br>
+     * this function returns zero (0).<br>
+     * @param {number|string} pNum A number or string that can be converted into a number
+     * @param {number|string} pDefault A number or string that can be converted into a number to be returned if the first argument is NaN or Infinity
+     * @returns {number} A valid number that is neither NaN nor Infinity
+     */
+    const resolveNullOrNaN = function( pNum, pDefault = 0 )
+    {
+        const me = this;
+
+        if ( !isNumeric( pNum ) )
+        {
+            modulePrototype.reportError( new IllegalArgumentError( ERROR_MSG_NON_NUMERIC ), ERROR_MSG_NON_NUMERIC, S_WARN, modulePrototype.calculateErrorSourceName( modName, me ) );
+
+            return resolveNullOrNaN( pDefault, 0 );
+        }
+
+        const num = isNaN( pNum ) || !isFinite( pNum ) ? NaN : asFloat( pNum );
+
+        if ( !isNanOrInfinite( num ) )
+        {
+            return num;
+        }
+
+        return resolveNullOrNaN( pDefault, 0 );
+    };
+
+    /**
+     * Returns true if the specified argument<br>
+     * is greater than (or equal to, if inclusive is true) the lower bound<br>
+     * and less than (or equal to, if inclusive is true) the upper bound.<br>
+     * <br>
+     * The default for inclusive is true.<br>
+     * <br>
+     * @param {number|string} pNum A number (or string that can be converted into a number) to evaluate
+     * @param {number|string} pLowerBound A number (or string that can be converted into a number) that the first argument must be greater than or equal to
+     * @param {number|string} pUpperBound A number (or string that can be converted into a number) that the first argument must be less than or equal to
+     * @param {boolean} [pInclusive=true] A boolean indicating whether the number must be strictly greater than the lower bound and less than the upper bound or can also be equal to either
+     * @returns {boolean} true if the specified value is greater than or equal to the lower bound and less than or equal to the upper bound
+     */
+    const isBetween = function( pNum, pLowerBound, pUpperBound, pInclusive = true )
+    {
+        const num = resolveNullOrNaN( pNum );
+
+        const lower = resolveNullOrNaN( pLowerBound );
+        const upper = resolveNullOrNaN( pUpperBound );
+
+        const inclusive = !!pInclusive;
+
+        return inclusive ? ((num >= lower) && (num <= upper)) : ((num > lower) && (num < upper));
+    };
+
+    /**
+     * @typedef DivisionOptions
+     * @property {number} byZero The value to return if the divisor is zero (0)
+     * @property {number} [defaultDividend=1] The value to use as the dividend, or numerator, when the specified value is NaN or Infinity
+     * @property {number} [defaultDivisor=1] The value to use as the divisor, or denominator, when the specified value is NaN or Infinity
+     * @property {number} [defaultQuotient=0] The value to return if the result is NaN or Infinity
+     */
+
+    const DEFAULT_OPTIONS_DIVISION =
+        {
+            byZero: Infinity,
+            defaultDividend: 1,
+            defaultDivisor: 1,
+            defaultQuotient: 0,
+        };
+
+    /**
+     * Returns a valid result of dividing the first argument by the second,<br>
+     * handling division by zero or invalid arguments<br>
+     * as defined by the options specified.<br>
+     * <br>
+     * @see {@link DivisionOptions}
+     *
+     * @param {number|string} pDividend A number which will be divided by the second argument
+     *
+     * @param {number|string} pDivisor  A number which will be used as the divisor, or denominator.<br>
+     *                                  That is, this is the value by which the dividend will be divided.<br>
+     *
+     * @param {DivisionOptions|object} pOptions An object providing one or more of the properties<br>
+     *                                          defined in the DivisionOptions type definition<br>
+     *                                          to define how to handle Null, NaN, or Infinity<br>
+     *                                          or attempted division by zero.<br>
+     *
+     * @returns {number} The result of dividing the first argument by the second,<br>
+     *                   gracefully handling invalid arguments or attempted division by zero (0)
+     */
+    const quotient = function( pDividend, pDivisor, pOptions = DEFAULT_OPTIONS_DIVISION )
+    {
+        const me = this;
+
+        const options = populateOptions( pOptions, DEFAULT_OPTIONS_DIVISION );
+
+        const dividend = resolveNullOrNaN( pDividend, resolveNullOrNaN( options.defaultDividend ) );
+
+        if ( isZero( dividend ) )
+        {
+            return 0;
+        }
+
+        const divisor = resolveNullOrNaN( pDivisor, resolveNullOrNaN( options.defaultDivisor ) );
+
+        if ( isZero( divisor ) )
+        {
+            modulePrototype.reportError( new IllegalArgumentError( ERROR_MSG_DIVISION_BY_ZERO ), ERROR_MSG_DIVISION_BY_ZERO, S_WARN, modulePrototype.calculateErrorSourceName( modName, me ) );
+
+            return resolveNullOrNaN( options.byZero, resolveNullOrNaN( options.defaultQuotient, 0 ) );
+        }
+
+        if ( 1 === divisor )
+        {
+            return resolveNullOrNaN( dividend, resolveNullOrNaN( options.defaultDividend ) );
+        }
+
+        return resolveNullOrNaN( dividend / divisor, resolveNullOrNaN( options.defaultQuotient ) );
+    };
+
+
 
     let mod =
         {
@@ -117,6 +279,10 @@ const $scope = constants?.$scope || function()
             toBinary,
             asInt,
             asFloat,
+            resolveNullOrNaN,
+            isBetween,
+            quotient,
+
         };
 
     mod = modulePrototype.extend( mod );
