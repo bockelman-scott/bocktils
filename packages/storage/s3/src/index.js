1952e635962ece64dff33a4e0c13eb68474010ff
bockelman-scott <152332497+bockelman-scott@users.noreply.github.com>
2026-08-21 23:34:18 -0500

    packaged s3 blob client facade

---
+++ packages/storage/s3/src/index.js
@@ -0,0 +1,24 @@
+(function exposePackage()
+{
+    const { BlobStorageClient } = require( "@toolbocks/storage" );
+
+    const { S3BlobStorageClient } = require( "../src/S3BlobStorageClient.js" );
+
+    const mod =
+        {
+            classes:
+                {
+                    BlobStorageClient,
+                    S3BlobStorageClient
+                },
+            BlobStorageClient,
+            S3BlobStorageClient
+        };
+
+    if ( "undefined" !== typeof module )
+    {
+        module.exports = Object.freeze( mod );
+    }
+
+    return Object.freeze( mod );
+}());
